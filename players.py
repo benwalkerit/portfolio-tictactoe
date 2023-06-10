5d4c3ab288a39c701c0af97e5c8896aa96bc08ee
benwalkerit <benrwalker@icloud.com>
2023-06-10 22:33:57 +0100

    final version

--- players.py
+++
@@ -1,8 +0,0 @@
-#Create Players
-PLAYERS = 1
-class Player:
-    
-    def __init__(self, player_count):
-        self.name = input('Please enter the players name: \n')
-        self.score = 0
-    
